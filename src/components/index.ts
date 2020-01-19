dae8792fd8255cc4d9a34722c265f23a61a7e614
沈经纬 <327538014@qq.com>
2020-01-19 15:16:37 +0800

    fix(layout): add global entry of layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from "./button";
 export { default as ConfigProvider } from "./config-provider";
 export * from "./menu";
+export * from "./layout";
 
 export * from "./utils/colors";
